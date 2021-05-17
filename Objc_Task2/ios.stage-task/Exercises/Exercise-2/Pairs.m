c12003de0c7b94bb008fb14ecd891d09d57efd26
Yaroslav3452 <yaroslav_poluyan@mail.ru>
2021-05-17 16:44:19 +0300

    second commit

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -5,8 +5,8 @@
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
     int count = 0;
     for( int i = 0; i < [array count]; i++){
-        for(int j = 0; j <[array count]; j++){
-            if([[array objectAtIndex:i] intValue] - [[array objectAtIndex:j] intValue] == [number intValue]){
+        for(int j = i + 1; j < [array count]; j++){
+            if(ABS([[array objectAtIndex:i] intValue] - [[array objectAtIndex:j] intValue]) == [number intValue]){
                 count++;
             }
         }
