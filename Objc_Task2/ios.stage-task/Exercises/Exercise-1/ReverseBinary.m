15f09ffb9737a7079f1705d9da52489c640094f9
alexeykachan03 <71043125+alexeykachan03@users.noreply.github.com>
2021-05-18 01:46:08 +0300

    Update ReverseBinary.m

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -5,16 +5,16 @@ UInt8 ReverseInteger(UInt8 n) {
     NSMutableString *binary = [[NSMutableString alloc] init];
     NSMutableString *reversed = [[NSMutableString alloc] init];
     for (NSInteger i = 0; i < 8; i++) {
-        [binary insertString:([NSString stringWithFormat:@"%d", n%2]) atIndex:0];
+            [binary insertString:([NSString stringWithFormat:@"%d", n%2]) atIndex:0];
         [reversed appendString:([NSString stringWithFormat:@"%d", n%2])];
         n = n / 2;
-    }
-    for (NSInteger i = 0; i < 8; i++) {
-        NSString *singleCharSubstring = [reversed substringWithRange:NSMakeRange((7-i), 1)];
-        NSInteger *x = [singleCharSubstring intValue];
-        if (x == 1) {
-        result += pow(2, i);
-        }
-    }
+                    }
+        for (NSInteger i = 0; i < 8; i++) {
+            NSString *singleCharSubstring = [reversed substringWithRange:NSMakeRange((7-i), 1)];
+            NSInteger *x = [singleCharSubstring intValue];
+            if (x == 1) {
+            result += pow(2, i);
+                                }
+                                    }
     return result;
 }
